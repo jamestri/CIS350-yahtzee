74bb8f8438f878d7ed1b91fe54a5f7766a5e09e7
jamestri <jamestri@mail.gvsu.edu>
2019-12-04 11:19:32 -0500

    Generated JavaDocs

---
+++ JavaDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Die"},{"p":"<Unnamed>","l":"GameLogic"},{"p":"<Unnamed>","l":"GameStatus"},{"p":"<Unnamed>","l":"GUI"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"ScoreOption"},{"p":"<Unnamed>","l":"Testing"}]
